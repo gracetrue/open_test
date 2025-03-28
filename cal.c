19c04bd037cb1532f874167a30c09ad7c2a4ce9a
dmsehd990815 <dmsehd990815@gmail.com>
2025-03-28 13:40:34 +0900

    edit cal.c

--- cal.c
+++ cal.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(){
-	int num1, num2;
+	int num1, num2, num3;
 
 	num1=3;
 	num2=5;
+	num3=2;
 
-	printf("%d + %d = %d\n", num1, num2, num1+num2);
+	printf("%d + %d + %d = %d\n", num1, num2, num3, num1+num2+num3);
 	retrun 0;
 }
