df277f9e93fac1b99268a17f5cc182f931f335f9
gracetrue <dw0815@chungbuk.ac.kr>
2026-03-20 11:15:56 +0900

    Updated files

--- class0404.py
+++
@@ -1,5 +0,0 @@
-print("class branch test")
-
-print("edit class0404.py content...")
-
-print("I want to go to home")
--- test2.py
+++
@@ -1,6 +0,0 @@
-
-
-2e44391 (HEAD -> main) Add main commit 4
-3d04d7a Add master commit 3
-2f0d65d abcdefg
-
